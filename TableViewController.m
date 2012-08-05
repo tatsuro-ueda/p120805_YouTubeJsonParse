ba33feb19c45a92e00781352d8ba99956201b63f
weed <weed_7777@yahoo.co.jp>
2012-08-05 19:51:28 +0900

    simplify codes

--- TableViewController.m
+++ TableViewController.m
@@ -140,88 +140,14 @@ const NSString *kStrJsonURL = @"http://gdata.youtube.com/feeds/api/videos?start-
 }
 
 - (IBAction)refresh:(id)sender {
-    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"読み込んでいます"
-                                                        message:nil//@"\n\n"
-                                                       delegate:self
-                                              cancelButtonTitle:@"キャンセル"
-                                              otherButtonTitles:nil];
-//    _progressView = [[UIProgressView alloc]
-//                     initWithFrame:CGRectMake(30.0f, 60.0f, 225.0f, 90.0f)];
-    [alertView addSubview:_progressView];
-//    [_progressView setProgressViewStyle: UIProgressViewStyleBar];
-    [alertView show];
-    
-    // JSONを取得する
     NSURL *url = [NSURL URLWithString:kStrJsonURL];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id getJSON) {
         _JSON = getJSON;
         NSLog(@"%@", _JSON);
-        
-        // JSONを取得し終わったらアラートビューを閉じてテーブルを更新する
-        [alertView dismissWithClickedButtonIndex:0 animated:YES];
         [self.tableView reloadData];
     } failure:nil];
     [operation start];        
-}
-
-- (NSURL *)ogImageURLWithURL:(NSURL *)url
-{
-    NSString *string = [self encodedStringWithContentsOfURL:url];    
-    
-    // prepare regular expression to find text
-    NSError *error   = nil;
-    NSRegularExpression *regexp =
-    [NSRegularExpression regularExpressionWithPattern:
-     @"<meta property=\"og:image\" content=\".+\""
-                                              options:0
-                                                error:&error];
-    
-    // find by regular expression
-    NSTextCheckingResult *match =
-    [regexp firstMatchInString:string options:0 range:NSMakeRange(0, string.length)];
-    
-    // get the first result
-    NSRange resultRange = [match rangeAtIndex:0];
-//    NSLog(@"match=%@", [string substringWithRange:resultRange]); 
-    
-    if (match) {
-        
-        // get the og:image URL from the find result
-        NSRange urlRange = NSMakeRange(resultRange.location + 35, resultRange.length - 35 - 1);
-        NSURL *urlOgImage = [NSURL URLWithString:[string substringWithRange:urlRange]];
-        return urlOgImage;
-    }
-    return nil;
-}
-
-- (NSString *)encodedStringWithContentsOfURL:(NSURL *)url
-{
-    // Get the web page HTML
-    NSData *data = [NSData dataWithContentsOfURL:url];
-    
-    // response
-    int enc_arr[] = {
-        NSUTF8StringEncoding,           // UTF-8
-        NSShiftJISStringEncoding,       // Shift_JIS
-        NSJapaneseEUCStringEncoding,    // EUC-JP
-        NSISO2022JPStringEncoding,      // JIS
-        NSUnicodeStringEncoding,        // Unicode
-        NSASCIIStringEncoding           // ASCII
-    };
-    NSString *data_str = nil;
-    int max = sizeof(enc_arr) / sizeof(enc_arr[0]);
-    for (int i=0; i<max; i++) {
-        data_str = [
-                    [NSString alloc]
-                    initWithData : data
-                    encoding : enc_arr[i]
-                    ];
-        if (data_str!=nil) {
-            break;
-        }
-    }
-    return data_str;    
 }
 
 @end
